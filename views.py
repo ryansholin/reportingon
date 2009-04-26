379a738248ca14ce72c552a604029ff7c2447a17
intellinick <intellinick@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-04-26 22:47:24 +0000

    getting some initial structure up

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("hi")
+    return render_to_response('home_not_logged_in.html', locals(), context_instance=RequestContext(request))

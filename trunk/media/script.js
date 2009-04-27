a6c8796ff2c0cc13c94147d41b5bf9e6bd5773e5
intellinick <intellinick@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-04-27 00:09:26 +0000

    some initial styles for frame

--- trunk/media/script.js
+++ trunk/media/script.js
@@ -1,3 +1,10 @@
 $(function() {
-    
+    if ($("#search input").length) {
+        $("#search input").focus(function() {
+            if ($(this).attr('value') == 'Search...') {$(this).attr('value','')}
+        });
+        $("#search input").blur(function() {
+            if ($(this).attr('value') == '') {$(this).attr('value','Search...')}
+        });
+    }
 });

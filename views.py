b4c181ae26708a96977e6a72874c6fbf03eef4ac
intellinick <intellinick@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-04-27 02:08:31 +0000

    removing test

--- views.py
+++ views.py
@@ -3,5 +3,4 @@ from django.template import RequestContext
 from django.http import HttpResponse
 
 def home(request):
-    assert False, 'test'
     return render_to_response('home_not_logged_in.html', locals(), context_instance=RequestContext(request))

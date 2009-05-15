93f6585be60bb3ad5a8ae231ba19b2142e4f41d0
intellinick <intellinick@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-05-15 03:56:16 +0000

    First live iteration of working Question CRUD. Mostly based on Petes code; just had to tweak things here and there and get the UI workflow down.

--- questions/models.py
+++ questions/models.py
@@ -1,58 +1,38 @@
-from django.db import models
-from django.db.models import permalink
 from django.utils.translation import ugettext_lazy as _
-from django.contrib import admin
 from django.contrib.auth.models import User
+from django.db.models import permalink
+from django.contrib import admin
+from datetime import datetime
+from django.db import models
 
 import tagging
-from tagging.models import Tag
 from tagging.fields import TagField
-
-from datetime import datetime # for date defaults
+from tagging.models import Tag
 
 class Question(models.Model):
     """Question model"""
+    
     STATUS_CHOICES = (
-        (1, _('Published')),
+        (1, _('Open')),
         (2, _('Closed')),
     )
-    title           = models.CharField(_('title'), max_length=250)
-    slug            = models.SlugField(_('slug'), editable=False, null=False, blank=False)
-    author          = models.ForeignKey(User, blank=False, null=False)
-    body            = models.TextField(_('body'), blank=False)
-    status          = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=1)
-    published       = models.DateTimeField(_('publish'), default=datetime.now)
-    created         = models.DateTimeField(_('created'), auto_now_add=True,)
-    modified        = models.DateTimeField(_('modified'), auto_now=True)
-    tags            = TagField()
-    # objects         = QuestionManager()
+    
+    question    = models.TextField(_('question'))
+    slug        = models.SlugField(_('slug'), editable=False, null=False, blank=False)
+    author      = models.ForeignKey(User, blank=False, null=False)
+    status      = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=1)
+    created     = models.DateTimeField(_('created'), auto_now_add=True)
+    modified    = models.DateTimeField(_('modified'), auto_now=True)
+    tags        = TagField()
 
     class Meta:
-        verbose_name = _('question')
-        verbose_name_plural = _('questions')
-        db_table  = 'ro_questions'
-        ordering  = ('-modified',)
-        get_latest_by = 'published'
-    
-    # we'll use sphinx later on because it's sick    
-    # search = SphinxSearch(
-    #        index ='questions', 
-    #        weights = { # individual field weighting
-    #            'title': 100,
-    #            'tags': 80,
-    #            'body': 50,
-    #        }
-    #    )
-
-    class Admin:
-        pass
+        verbose_name = _('Question')
+        verbose_name_plural = _('Questions')
+        ordering  = ('-created',)
 
     def __unicode__(self):
-        return u'%s' % self.title
-           
-    # once the view is created, unleash this puppy
-    # @permalink 
-    # def get_absolute_url(self): 
-    #     return ('question_detail', None, {
-    #         'slug': self.slug,
-    #         })
+        return u'%s' % self.question
+    
+    @permalink 
+    def get_absolute_url(self): 
+        return ('question_view', None, {'slug': self.slug})

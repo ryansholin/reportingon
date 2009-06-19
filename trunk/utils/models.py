d17f1459e3daca0b18a4cca7a492922879204929
intellinick <intellinick@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-06-19 20:07:37 +0000

    let there be gravatars

--- trunk/utils/models.py
+++ trunk/utils/models.py
@@ -6,6 +6,8 @@ from reportingon.rated.models import Rated
 from django.contrib.auth.models import User
 from django.db.models import signals
 
+import md5
+
 class UserProfile(models.Model):
     user        = models.ForeignKey(User, unique=True)
     affiliation = models.CharField(max_length=200, blank=True)
@@ -14,6 +16,9 @@ class UserProfile(models.Model):
     
     def get_score(self):
         return Rated.objects.filter(rated_user=self.user).count()
+    
+    def get_gravatar(self):
+        return md5.new(User.objects.get(id__exact=self.user.id).email.lower()).hexdigest()
 
 def user_post_save(sender, instance, **kwargs):
     profile, new = UserProfile.objects.get_or_create(user=instance)

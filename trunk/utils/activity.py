97817181782b7f0d9c7648a4402fd01b1ddc4b96
intellinick <intellinick@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-06-26 03:31:55 +0000

    - Fixed non-logged-in issues with trying to rate / watch items - Fix bug with user.is_authenticated (in that it should be request.user) - Fixed content URLs in activity feed - Pagination for user activity - Changed activity count to 10 (imo, makes page seem more concise - let me know if you want it changed back) - Baseline func for watched page

--- trunk/utils/activity.py
+++ trunk/utils/activity.py
@@ -4,60 +4,62 @@ from django.contrib.comments.models import Comment
 
 # requires 'user' object
 def get_recent_activity_for_user(user, sort=False, thirdPerson=False):
-    user_url = '/users/%s' % user.username
     recent_activity = list()
     for obj in Watched.objects.filter(user=user):
         if obj.content_type.model == 'user': # user is watching a user
-            description = """%s watching <a href="#">%s</a>""" % ("""<a href="%s">%s</a> started""" % (user_url, user.username) if thirdPerson else 'Started', obj.object.username)
+            type = 'user-watched-user'
+            description = """%s watching <a href="%s">%s</a>""" % ("""<a href="%s">%s</a> started""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Started', obj.object.get_absolute_url(), obj.object.username)
         elif obj.content_type.model == 'tag':
-            tag_url = '/tags/%s' % obj.object.name
-            description = """%s watching the &ldquo;<a href="#">%s</a>&rdquo; beat""" % ("""<a href="%s">%s</a> started""" % (tag_url, user.username) if thirdPerson else 'Started', obj.object.name)
+            type = 'user-watched-beat'
+            tag_url = '/beats/%s' % obj.object.name
+            description = """%s watching the &ldquo;<a href="%s">%s</a>&rdquo; beat""" % ("""<a href="%s">%s</a> started""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Started', tag_url, obj.object.name)
         elif obj.content_type.model == 'question':
-            question_url = '/questions/%s/%s' % (obj.object.id, obj.object.slug)
-            description = """%s watching the question &ldquo;<a href="#">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> started""" % (question_url, user.username) if thirdPerson else 'Started', obj.object.question)
+            type = 'user-watched-question'
+            description = """%s watching the question &ldquo;<a href="%s">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> started""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Started', obj.object.get_absolute_url(), obj.object.question)
         elif obj.content_type.model == 'savedsearch':
-            # search_url = '/?'
-            description = """%s watching the search &ldquo;<a href="#">%s</a>&rdquo;""" % ("""<a href="#">%s</a> started""" % user.username if thirdPerson else 'Started', obj.object.query)
+            type = 'user-watched-search'
+            search_url = '/search?query=' + obj.object.query
+            description = """%s watching the search &ldquo;<a href="%s">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> started""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Started', search_url, obj.object.query)
         else:
+            type = 'fail'
             continue
-        recent_activity.append({ 'description': description, 'date': obj.created })
+        recent_activity.append({ 'description': description, 'date': obj.created , 'type': type})
         description = ''
         
     for obj in Question.objects.filter(author=user):
-        recent_activity.append({ 'description': """%s &ldquo;<a href="#">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> asked""" % (user_url, user.username) if thirdPerson else 'Asked', obj.question), 'date': obj.created })
+        type = 'question'
+        recent_activity.append({ 'description': """%s &ldquo;<a href="%s">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> asked""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Asked', obj.get_absolute_url(), obj.question), 'date': obj.created, 'type': type, 'question': obj })
     
     for obj in Comment.objects.filter(user=user):
+        type = 'answer'
         try:
             question = Question.objects.get(id=obj.object_pk)
-            question_url = '/questions/%s/%s' % (question.id, question.slug)
-            recent_activity.append({ 'description': """%s a <a href="%s">question</a> with &ldquo;<a href="%s">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> answered""" % (user_url, user.username) if thirdPerson else 'Answered', question_url, question_url, obj.comment), 'date': obj.submit_date })
+            recent_activity.append({ 'description': """%s a <a href="%s">question</a> with &ldquo;<a href="%s">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> answered""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Answered', question.get_absolute_url(), question.get_absolute_url() + '#answer-' + str(obj.id), obj.comment), 'date': obj.submit_date, 'type': type })
         except:
             pass
-    
-    if sort:
-        recent_activity.sort(key=lambda x:x['date'], reverse=True)
     
     return recent_activity
     
 # requires a 'question' object
 def get_recent_activity_for_question(question, sort=False, thirdPerson=False):
     """
-        single question has a creation date, date_updated, and answers to that question
+    single question has a creation date, date_updated, and answers to that question
     """
     recent_activity = list()
     
     # add original question asking
-    user_url = "/users/%s" % question.author.username
-    question_url = '/questions/%s/%s' % (question.id, question.slug)
-    recent_activity.append({ 'description': """<a href="%s">%s</a> asked &ldquo;<a href="%s">%s</a>&rdquo;""" % (user_url, question.author.username, question_url, question.question), 'date': question.created })
+    type = 'user-asked-question'
+    recent_activity.append({ 'description': """<a href="%s">%s</a> asked &ldquo;<a href="%s">%s</a>&rdquo;""" % (question.author.get_absolute_url(), question.author.username, question.get_absolute_url(), question.question), 'date': question.created, 'type': type })
     
     # add update if created != modified
+    type = 'user-updated-question'
     if(question.created is not question.modified):
-        recent_activity.append({ 'description': """<a href="%s">%s</a> updated a question &ldquo;<a href="%s">%s</a>&rdquo;""" % (user_url, question.author.username, question_url, question.question), 'date': question.modified })
+        recent_activity.append({ 'description': """<a href="%s">%s</a> updated a question &ldquo;<a href="%s">%s</a>&rdquo;""" % (question.author.get_absolute_url(), question.author.username, question.get_absolute_url(), question.question), 'date': question.modified, 'type': type })
         
     for answer in Comment.objects.filter(object_pk=question.id):
         if answer.user_id:
-            recent_activity.append({ 'description': """<a href="#">%s</a> posted an answer to &ldquo;<a href="#">%s</a>&rdquo;""" % (answer.user.username, question.question), 'date': answer.submit_date })
+            type = 'user-answered-question'
+            recent_activity.append({ 'description': """<a href="%s">%s</a> posted an answer to &ldquo;<a href="%s">%s</a>&rdquo;""" % (answer.user.get_absolute_url(), answer.user.username, question.get_absolute_url() + '#answer-' + str(answer.id), question.question), 'date': answer.submit_date, 'type': type })
             
     if sort:
         recent_activity.sort(key=lambda x:x['date'], reverse=True)

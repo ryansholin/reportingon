43e93d58fd25fdb677dfdc2cecba9a20bf115b1e
pete.karl <pete.karl@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-06-30 02:21:54 +0000

    I think we have a win (that is, dupes are fixed)

--- utils/activity.py
+++ utils/activity.py
@@ -3,7 +3,7 @@ from reportingon.watched.models import Watched, SavedSearch
 from django.contrib.comments.models import Comment
 
 # requires 'user' object
-def get_recent_activity_for_user(user, sort=False, thirdPerson=False):
+def get_recent_activity_for_user(user, sort=False, thirdPerson=False, unique=False):
     recent_activity = list()
     watched_beats = list(Watched.objects.filter(user=user, content_type__model='tag'))
     watched_questions = list(Watched.objects.filter(user=user, content_type__model='question'))
@@ -49,14 +49,17 @@ def get_recent_activity_for_user(user, sort=False, thirdPerson=False):
             recent_activity.append({ 'description': """%s a <a href="%s">question</a> with &ldquo;<a href="%s">%s</a>&rdquo;""" % ("""<a href="%s">%s</a> answered""" % (user.get_absolute_url(), user.username) if thirdPerson else 'Answered', question.get_absolute_url(), question.get_absolute_url() + '#answer-' + str(obj.id), obj.comment), 'date': obj.submit_date, 'type': type, 'id': type + str(question.id), 'watched_user': user, 'answer': obj, 'question': question })
         except:
             pass
-    
-    unique_activity = uniqueify(recent_activity, lambda x:x['id'])
-    unique_activity.sort(key=lambda x:x['date'], reverse=True)
+        
+    if unique:
+        recent_activity = uniqueify(recent_activity, lambda x:x['id'])
+        
+    if sort:    
+        recent_activity.sort(key=lambda x:x['date'], reverse=True)
 
-    return unique_activity
+    return recent_activity
     
 # requires a 'question' object
-def get_recent_activity_for_question(question, sort=False, thirdPerson=False):
+def get_recent_activity_for_question(question, sort=False, thirdPerson=False, unique=False):
     """
     single question has a creation date, date_updated, and answers to that question
     """
@@ -76,6 +79,9 @@ def get_recent_activity_for_question(question, sort=False, thirdPerson=False):
             type = 'answer'
             recent_activity.append({ 'description': """<a href="%s">%s</a> posted an answer to &ldquo;<a href="%s">%s</a>&rdquo;""" % (answer.user.get_absolute_url(), answer.user.username, question.get_absolute_url() + '#answer-' + str(answer.id), question.question), 'date': answer.submit_date, 'type': type, 'id': type + str(question.id),'watched_user': answer.user, 'answer': answer, 'question': question })
             
+    if unique:
+        recent_activity = uniqueify(recent_activity, lambda x:x['id'])
+    
     if sort:
         recent_activity.sort(key=lambda x:x['date'], reverse=True)
         

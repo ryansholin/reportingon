432e7b905d1bc9b611f9db01e495cab5fee34969
pete.karl <pete.karl@608e5136-ebf0-11dd-b90f-f9a5cbf66604>
2009-07-02 20:47:46 +0000

    pushing up fix to keep users from being notified of their own answers

--- trunk/signals.py
+++ trunk/signals.py
@@ -3,22 +3,23 @@ from django.contrib.sites.models import Site
 from django.core.mail import send_mail, EmailMultiAlternatives
 
 def question_has_new_answer(sender, comment, request, *args, **kwargs):
-    site = Site.objects.get_current()
-    question = comment.content_object
-    commenter_url = """<a href="http://%s/users/%s">%s</a>""" % (site.domain, comment.user_name, comment.user_name) if comment.user_id is not 'NULL' else ''
-    question_url = """http://%s/questions/%s/%s""" % (site.domain, question.id, question.slug)
-    question_link = """<a href="%s">go to question</a>""" % question_url
+    if comment.content_object.author.id != comment.user_id:
+        site = Site.objects.get_current()
+        question = comment.content_object
+        commenter_url = """<a href="http://%s/users/%s">%s</a>""" % (site.domain, comment.user_name, comment.user_name) if comment.user_id is not 'NULL' else ''
+        question_url = """http://%s/questions/%s/%s""" % (site.domain, question.id, question.slug)
+        question_link = """<a href="%s">go to question</a>""" % question_url
     
-    subject = "[ReportingOn] Your question has a new answer from %s! (%s)" % (comment.user_name, str(question))
-    text_message = """You asked: "%s"\n\n%s answered with: "%s"\n\nSee the question: %s""" % (str(question), comment.user_name, comment.comment, question_url)
-    html_message = """<p style="color:#b9b9b9;font-size:16px">Your question</p>
-                 <p style="color:#666">%s</p>
-                 <br/>
-                 <p style="color:#b9b9b9;font-size:16px">%s answered with:</p>
-                 <p style="color:#222""><strong>%s</strong><br/><br/>%s</p>""" % (str(question), commenter_url, comment.comment, question_link)
-    from_email = settings.DEFAULT_FROM_EMAIL    
-    to = question.author.email
+        subject = "[ReportingOn] Your question has a new answer from %s! (%s)" % (comment.user_name, str(question))
+        text_message = """You asked: "%s"\n\n%s answered with: "%s"\n\nSee the question: %s""" % (str(question), comment.user_name, comment.comment, question_url)
+        html_message = """<p style="color:#b9b9b9;font-size:16px">Your question</p>
+                     <p style="color:#666">%s</p>
+                     <br/>
+                     <p style="color:#b9b9b9;font-size:16px">%s answered with:</p>
+                     <p style="color:#222""><strong>%s</strong><br/><br/>%s</p>""" % (str(question), commenter_url, comment.comment, question_link)
+        from_email = settings.DEFAULT_FROM_EMAIL    
+        to = question.author.email
 
-    msg = EmailMultiAlternatives(subject, text_message, from_email, [to])
-    msg.attach_alternative(html_message, "text/html")
-    msg.send()
+        msg = EmailMultiAlternatives(subject, text_message, from_email, [to])
+        msg.attach_alternative(html_message, "text/html")
+        msg.send()
